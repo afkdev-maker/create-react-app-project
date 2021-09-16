a9e3f28d483c87b70cc7040f50850a8e4fdf409c
C#DEV <5483117+csharpdev-afk@users.noreply.github.com>
2021-09-16 15:26:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./App.css";
+import Footer from './footer';
 
 function App() {
   const [date, setDate] = useState(null);
@@ -14,25 +15,10 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Show me the money</h1>
       <h2>Deployed with 4everland!</h2>
-      <p>
-        This project was bootstrapped with{" "}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{" "}
-        and contains three directories, <code>/public</code> for static assets,{" "}
-        <code>/src</code> for components and content, and <code>/api</code>{" "}
-        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
-        function. See{" "}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : "Loading date..."}</p>
+      <Footer/>
     </main>
   );
 }
